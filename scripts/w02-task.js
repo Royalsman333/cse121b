41fbcd6985bd837bf8f058b082793b59541a0a11
Royalsman333 <146398098+Royalsman333@users.noreply.github.com>
2024-01-23 21:26:08 -0600

    update2.0

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,32 +3,33 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = 'Jeremy Untch';
-let currentYear = currentDateAndTime.getFullYear();
-let profilePicture = 'images/profilepic.jpg';
+const fullName = "Jeremy Untch";
+const currentYear = new Date().getFullYear();
+const profilePicture = "images/profilepic.jpg";
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('id');
+const foodElement = document.querySelector('#food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('#profilePicture');
-
+const imageElement = document.querySelector('img');
+const nameElement = document.getElementById('name');
 /* Step 4 - Adding Content */
-nameElement.innerHTML = '<strong>${fullName}</strong>';
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', 'Profile image of ' + fullName);
+imageElement.setAttribute('alt', `Profile Image of ${fullName}.`);
 
 /* Step 5 - Array */
-let favoriteFoods = ['Ribs', 'Dr. Pepper', 'Strawberries', 'Bananas'];
-foodElement.textContent = 'My favorite foods: ' + favoriteFoods.join(', ');
-let singleFavoriteFood = 'Hamburgers';
-favoriteFoods.push(singleFavoriteFood);
-foodElement.innerHTML += '<br>${favFoods}';
-favoriteFoods.shift();
-outputElement.innerHTML = 'Modified Array:<br>' + favoriteFoods.join('<br>');
-favoriteFoods.pop();
-outputElement.innerHTML = 'Final Modified Array:<br>' + favoriteFoods.join('br');
+let favfoods = ["Steak", "Hamburgers", "Cereal", "Ribs", "Dr. Pepper", "Strawberries", "Bananas"];
+
+foodElement.innerHTML += favfoods;
+let newfood = 'Turkey';
+favfoods.push(newfood);
+foodElement.innerHTML += `<br>${favfoods}`;
+favfoods.shift();
+foodElement.innerHTML += `<br>${favfoods}`;
+favfoods.pop();
+foodElement.innerHTML += `<br>${favfoods}`;
 
 
 
