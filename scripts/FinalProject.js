f1ba909b2e7fbdd3a3814e30b173cc9c40ac2041
Royalsman333 <146398098+Royalsman333@users.noreply.github.com>
2024-02-22 21:50:24 -0600

    Final Project take 1

---
+++ scripts/FinalProject.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const usersList = document.getElementById("users-list")
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => {
+            users.forEach(user => {
+                const li = document.createElement("li");
+                li.innerHTML = `
+                    <strong>Name:</strong> ${user.name} <br>
+                    <strong>Email:</strong> ${user.email} <br>
+                    <strong>Phone:</strong> ${user.phone}
+                `;
+                usersList.appendChile(li);
+            });
+        })
+        .catch(error => console.log("Error fetching data:", error));
+});
